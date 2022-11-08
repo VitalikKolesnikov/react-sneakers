e211249bb76873ecc37e4f27a528cd7a87a39712
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2022-11-08 18:33:42 +0300

    Закончил вёрстку проекта

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
     <div className="wrapper">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <h2>Корзина 
+          <img  className="removeBtn" src="/img/btn-remove.svg"></img>
+          </h2>
             <div className="items">
               <div className="cartItem">
                 <img width={80} height={80} src="/img/sneakers/1.jpg"/>
@@ -23,18 +25,21 @@ function App() {
                 </div>
                 <img className="removeBtn" src="/img/btn-remove.svg"></img>
               </div>
+            </div>
+            <div className="cartTotalBlock">
               <ul>
-                <li>
-                  <span></span>
-                  <div></div>
-                  <b></b>
-                </li>
-                <li>
-                  <span></span>
-                  <div></div>
-                  <b></b>
-                </li>
+                  <li className="itemPrice">
+                    <span>Итого:</span>
+                    <div></div>
+                    <b>21 498 RU</b>
+                  </li>
+                  <li className="itemPrice">
+                    <span>Налог 5%:</span>
+                    <div></div>
+                    <b>1074 RU</b>
+                  </li>
               </ul>
+              <button className="greenButton">Оформить <img src="/img/arrow.svg" alt="Arrow"></img></button>
             </div>
         </div>
       </div>
